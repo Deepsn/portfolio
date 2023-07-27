be89d6192b62bbe81c75f5d8386a01cc8276d9c2
Speed <75620590+Deepsn@users.noreply.github.com>
2023-07-27 18:04:44 +0000

    chore: navbar hover effect

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,10 +1,21 @@
-<div class="flex w-full my-7">
-	<div class="flex w-[55%] justify-evenly">
+<nav class="flex w-full my-7">
+	<ul class="flex w-[55%] justify-evenly">
 		<a href="/">~/</a>
 		<a href="/">~/random</a>
-	</div>
+	</ul>
 
-	<div class="flex left-0 w-[45%] justify-evenly">
+	<ul class="flex left-0 w-[45%] justify-evenly">
 		<p>random text to make space</p>
-	</div>
-</div>
+	</ul>
+</nav>
+
+<style>
+	a {
+		text-decoration: underline transparent;
+		transition: text-decoration-color 200ms;
+	}
+
+	a:hover {
+		text-decoration-color: white;
+	}
+</style>
