2f0a9bd494721a01d47847b9e5fe26cd8912d261
Speed <75620590+Deepsn@users.noreply.github.com>
2023-07-28 01:46:27 +0000

    feat: popup chore: dev icons

---
+++ src/components/Popup.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	export let iconName: string;
+	let enabled = false;
+	let popupX: number;
+
+	function handleMouseEnter({ x }: MouseEvent) {
+		popupX = x;
+		enabled = true;
+	}
+
+	function handleMouseLeave() {
+		enabled = false;
+	}
+</script>
+
+<div aria-hidden on:mouseenter={handleMouseEnter} on:mouseleave={handleMouseLeave}>
+	<slot />
+</div>
+
+<div
+	style="transform: translate({popupX}px, -35px);"
+	class="{enabled ? 'inline' : 'hidden'} absolute left-0 bg-neutral-800 p-1 rounded-full"
+>
+	<p>{iconName}</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,40 @@
-<div
-	style="transform: translateY(-50%);"
-	class="relative top-[50%] flex justify-center items-center"
->
-	<p class="text-3xl">hi this is a test of font</p>
+<script lang="ts">
+	import Popup from "@/components/Popup.svelte";
+
+	let languages = [
+		"lua",
+		"typescript",
+		"javascript",
+		"nodejs",
+		"nextjs-original",
+		"react-original",
+		"svelte",
+		"tailwindcss",
+		"webpack"
+	];
+</script>
+
+<div class="absolute top-[50%] translate-y-[-50%] w-full">
+	<div class="mx-auto w-max">
+		<h1 class="text-6xl">Hi👋</h1>
+		<p>My name is Speed</p>
+		<p>and i'm a Software engineer</p>
+	</div>
+</div>
+
+<div class="absolute bottom-[3%] w-full">
+	<div class="mx-auto w-max">
+		<p class="text-[13px]">bla bla bla technologies:</p>
+		<div class="flex mx-auto bg-neutral-900 rounded-full w-max p-2 px-3 gap-2 justify-evenly">
+			{#each languages as languageName}
+				<Popup iconName={languageName.replaceAll(/\-.+/g, "")}>
+					<i
+						class="flex justify-center devicon-{languageName.match('-')
+							? languageName
+							: languageName + '-plain'}"
+					/>
+				</Popup>
+			{/each}
+		</div>
+	</div>
 </div>
